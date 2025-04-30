989886c138b55eceead04daea95071a14d18a8fb
ivonzhang <zhangivon@gmail.com>
2025-04-30 22:57:43 +0800

    chore(test): use another way to import ui library

--- apps/frontend/dashboard/src/App.tsx
+++ apps/frontend/dashboard/src/App.tsx
@@ -1,4 +1,6 @@
 import { Input } from '@my-org/ui';
+// 使用另一种方式导入 ui 库
+import { Button } from '@my-org/ui/dist/esm';
 
 import './App.css'
 
@@ -7,6 +9,9 @@ function App() {
     <div>
       <div>使用了 @my-org/ui 库中的 Input 组件</div>
       <Input />
+      <div>
+        <Button onClick={() => alert("哈哈哈")}>哈哈哈哈</Button>
+      </div>
     </div>
   )
 }
